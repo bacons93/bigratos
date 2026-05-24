8ae8f54058867d1a7ae1bf4cf50749aa489cb7aa
Dylan <bacons93ss@gmail.com>
2026-05-24 14:54:14 -0400

    Add version helper arguments

--- c-src/rat-version.c
+++ c-src/rat-version.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("rat package tools dev build\n");
-    return 0;
+#define RAT_VERSION "0.1-dev"
+
+static void print_help(void) {
+    printf("rat-version - show BigRatOS rat package tools version\n\n");
+    printf("Usage:\n");
+    printf("  rat-version\n");
+    printf("  rat-version --version\n");
+    printf("  rat-version --help\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1 || strcmp(argv[1], "--version") == 0) {
+        printf("rat package tools %s\n", RAT_VERSION);
+        return 0;
+    }
+
+    if (strcmp(argv[1], "--help") == 0) {
+        print_help();
+        return 0;
+    }
+
+    fprintf(stderr, "error: unknown option '%s'\n", argv[1]);
+    fprintf(stderr, "try: rat-version --help\n");
+    return 1;
 }
